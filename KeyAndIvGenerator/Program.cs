8c48b41600780959de970a1693d2c7c892017567
Denys Olleik <dolleik@gmail.com>
2015-11-17 03:18:34 -0500

    initial

---
+++ KeyAndIvGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyAndIvGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RijndaelManaged rm = new RijndaelManaged();
+            rm.GenerateKey();
+            rm.GenerateIV();
+            string key = Convert.ToBase64String(rm.Key);
+            string iv = Convert.ToBase64String(rm.IV);
+        }
+    }
+}
